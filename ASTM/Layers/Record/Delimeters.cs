136d00c382cb484231b8f3f972ba7606d7676211
alireza <am808036@gmail.com>
2024-11-18 19:06:28 +0330

    add message header and message termination

---
+++ ASTM/Layers/Record/Delimeters.cs
@@ -0,0 +1,10 @@
+﻿namespace ASTM.Layers.Record;
+
+internal static class Delimeters
+{
+    public static readonly char CR = '\r';
+    public static readonly char Field = '|';
+    public static readonly char Repeat = '\\';
+    public static readonly char Component = '^';
+    public static readonly char Escape = '&';
+}
---
+++ ASTM/Layers/Record/Records/MessageTermination.cs
@@ -0,0 +1,8 @@
+﻿namespace ASTM.Layers.Record.Records;
+
+internal class MessageTermination
+{
+    public static readonly char RecordTypeId = 'L';
+    public string SequenceNumber { get; set; } = "1";
+    public string TerminationCode { get; set; } = string.Empty; // N or E
+}
